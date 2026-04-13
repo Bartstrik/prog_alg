b291867c64aead99c2d11683cabfabe596c3db7c
Bartstrik <bcstrik@gmail.com>
2026-04-13 20:32:26 +0200

    ex 8

--- exercises/08_MPI/mainMPI.cpp
+++ exercises/08_MPI/mainMPI.cpp
@@ -12,15 +12,23 @@ int main(int argc, char* argv[]) {
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
 	if (id == 0) {
-		std::array<char, 50> greeting;
+		constexpr size_t gr_sz = 50;
 
 		std::cout << "Process " << id << " receives greetings from " << p - 1 << " processes!" << std::endl;
 		for (int i = 1; i < p; i++) {
 			// TODO use MPI_Recv
+			char greeting[gr_sz] = "";
+			
+			MPI_Recv(&greeting, gr_sz, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			std::cout << greeting << std::endl;
 		}
 		std::cout << "Greetings, done!" << std::endl;
 	} else {
 		// TODO use MPI_Send
+		std::string greetings("greetings from " + std::to_string(id));
+
+		std::cout << id << " start send now!" << std::endl;
+		MPI_Send(greetings.c_str(), greetings.size(), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 	}
 
 	MPI_Finalize();
