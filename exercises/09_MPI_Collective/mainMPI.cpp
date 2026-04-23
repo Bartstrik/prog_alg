c2aad17c25ca319a40cde91ad3a8493fae317333
Bartstrik <bcstrik@gmail.com>
2026-04-23 14:46:58 +0200

    ex9

--- exercises/09_MPI_Collective/mainMPI.cpp
+++ exercises/09_MPI_Collective/mainMPI.cpp
@@ -58,6 +58,56 @@ static void process() {
 	show(p, id, v1, "start");
 
 	// TODO use MPI collective operations
+
+	v2.assign(v1.begin(), v1.end());
+	MPI_Bcast(v2.data(), p, MPI_INT, 0, MPI_COMM_WORLD);
+	show(p, id, v2, "MPI_Bcast");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	MPI_Scan(v2.data(), v1.data(), p, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Scan");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	v2.assign(v1.begin(), v1.end());
+	MPI_Allgather(v2.data(), 1, MPI_INT, v1.data(), 1, MPI_INT, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Allgather");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	v2.assign(v1.begin(), v1.end());
+	MPI_Allreduce(v2.data(), v1.data(), p, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Allreduce");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	v2.assign(v1.begin(), v1.end());
+	v1 = {0,0,0,0,0};
+	MPI_Exscan(v2.data(), v1.data(), p, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Exscan");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	v2.assign(v1.begin(), v1.end());
+	v1 = {0,0,0,0,0};
+	MPI_Alltoall(v2.data(), 1, MPI_INT, v1.data(), 1, MPI_INT, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Alltoall");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	v2.assign(v1.begin(), v1.end());
+	v1 = {0,0,0,0,0};
+	int a[] = {1,1,1,1,1};
+	MPI_Reduce_scatter(v2.data(), v1.data(), a, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Reduce_scatter");
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	v2.assign(v1.begin(), v1.end());
+	v1 = {0,0,0,0,0};
+	MPI_Reduce(v2.data(), v1.data(), p, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
+	show(p, id, v1, "MPI_Reduce");
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
